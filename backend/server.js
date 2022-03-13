c9d0b31b9ec74f49f51b72fd64f4ce0acc966a15
Trinh Huynh <47219486+TrinhHuynh98@users.noreply.github.com>
2022-03-13 14:40:25 +0700

    push to heroku

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/amazone', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazone', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
